73e5da8afe2e610eded6b768b019fe369cbfd6ad
Ian Macalinao <ian@giza.us>
2014-11-15 12:41:44 -0600

    emu subdir

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public'));
+app.use('/emu', express.static(__dirname + '/jsemulator'));
+
 
 app.get('/yo/:btn', function(req, res) {
   var send = req.query;
