a85726398b79a4c00c306d5d2b3b57909a4aabeb
Ian Macalinao <ian@giza.us>
2014-11-15 12:17:40 -0600

    procfile

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + 'public'));
 
@@ -9,6 +9,6 @@ app.get('/yo_cb', function(req, res) {
   io.emit('yo', req.query);
 });
 
-http.listen(3000, function() {
+server.listen(3000, function() {
   console.log('listening on *:3000');
 });
