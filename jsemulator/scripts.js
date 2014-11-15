de19a58c152bb2b77484ee75ae2b34e6a0c0fc64
Ian Macalinao <ian@giza.us>
2014-11-15 13:31:35 -0600

    Append moves

--- jsemulator/scripts.js
+++ jsemulator/scripts.js
@@ -2,6 +2,9 @@ var socket = io();
 
 socket.on('yo', function(yo) {
   console.log(yo);
+
+  $('#actions').prepend($('<li>' + yo.username + ' - ' + yo.btn + '</li>'));
+
   GameBoyKeyDown(yo.btn);
   setTimeout(function() {
     GameBoyKeyUp(yo.btn);
